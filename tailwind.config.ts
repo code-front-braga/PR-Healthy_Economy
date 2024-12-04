df6e922877f9f4e779e5e95900fb4a77b980fef1
Leonardo Braga <programer_frontbraga@outlook.com>
2024-12-04 11:54:57 -0300

    chore: defined project styles [font, colors and screens]

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,18 +1,34 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
 
 export default {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
-    },
-  },
-  plugins: [],
+	content: [
+		'./src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+		'./src/components/**/*.{js,ts,jsx,tsx,mdx}',
+		'./src/app/**/*.{js,ts,jsx,tsx,mdx}',
+	],
+	theme: {
+		extend: {
+			colors: {
+				snow: '#fffafa',
+				greenSpring: '#1affaf',
+				blackPearl: '#000b0d',
+				greenCyprus: '#154b52',
+				safetyOrange: '#ff6200',
+				input: '#404748',
+			},
+			fontFamily: {
+				alumni: '"Alumni Sans", sans-serif',
+				lovedBKing: '"Loved by the King", cursive',
+				loveYLSister: '"Love Ya Like A Sister", cursive',
+			},
+			screens: {
+				mobile360px: '360px',
+				mobile414px: '414px',
+				mobile480px: '480px',
+				mobile600px: '600px',
+				mobile640px: '640px',
+			},
+		},
+	},
+	plugins: [],
 } satisfies Config;
